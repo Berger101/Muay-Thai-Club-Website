c0ca66ce23c8cbe40894cea614d717211730677c
Erik Berger <berger.erik12@gmail.com>
2024-05-16 15:23:35 +0200

    Hid secret key

--- muaythai/settings.py
+++ muaythai/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4nfkyvxb-vj2&l0ha_@d5+cymtgqjf=qdqp&x4gx3qx@v(g6!k'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
