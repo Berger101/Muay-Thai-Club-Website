4e185f6b369d2067379b7634cc6c4e4f7d6f3254
Erik Berger <berger.erik12@gmail.com>
2024-05-16 15:51:49 +0200

    Register Models in the Admin Panel

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TrainingSession, Booking
 
-# Register your models here.
+
+class TrainingSessionAdmin(admin.ModelAdmin):
+    list_display = ('instructor', 'date', 'time')
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'session')
+
+
+admin.site.register(TrainingSession, TrainingSessionAdmin)
+
+
+admin.site.register(Booking, BookingAdmin)
