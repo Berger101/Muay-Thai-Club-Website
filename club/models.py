7db78d6c895bcaf3fa33fae6b3995ec714ef3774
Erik Berger <berger.erik12@gmail.com>
2024-05-16 15:42:25 +0200

    Defining models

--- club/models.py
+++ club/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TrainingSession(models.Model):
+    instructor = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    session = models.ForeignKey(TrainingSession, on_delete=models.CASCADE)
