9060de31ca45b0e2f1473c6c4c279ef901c58b82
Erik Berger <berger.erik12@gmail.com>
2024-05-17 16:22:40 +0200

    Updated TrainingSessionAdmin class with summernote

--- club/admin.py
+++ club/admin.py
@@ -3,16 +3,14 @@ from .models import TrainingSession, Booking
 from django_summernote.admin import SummernoteModelAdmin
 
 
+@admin.register(TrainingSession)
 class TrainingSessionAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('description',)
     list_display = ('instructor', 'date', 'time')
-    # search_fields = ['instructor']
-    # list_filter = ('status',)
-    # prepopulated_fields = {'slug': ('instructor',)}
-
-
-admin.site.register(TrainingSession, TrainingSessionAdmin)
+    search_fields = ['instructor', 'description']
+    list_filter = ('instructor', 'date')
+    # prepopulated_fields = {}
 
 
 class BookingAdmin(admin.ModelAdmin):
