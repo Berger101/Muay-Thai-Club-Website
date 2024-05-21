c2c823c6227a8f6424539857330135751c2807c9
Erik Berger <berger.erik12@gmail.com>
2024-05-21 14:51:44 +0200

    Changed url path to activities

--- club/urls.py
+++ club/urls.py
@@ -5,6 +5,6 @@ from .views import TrainingSessionListView, TrainingSessionDetailView
 
 #  urlpattern for TrainingSessionList class-based view (from club/views.py) named home.
 urlpatterns = [
-    path('activities/', TrainingSessionListView.as_view(), name='home'),
+    path('', TrainingSessionListView.as_view(), name='activities'),
     path('activities/<int:pk>/', TrainingSessionDetailView.as_view(), name='training_session_detail'),
 ]
