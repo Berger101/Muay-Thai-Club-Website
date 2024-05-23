f33c1859e4ca36f7a0e28383ee1f8629fd8314f1
Erik Berger <berger.erik12@gmail.com>
2024-05-23 15:32:58 +0200

    Changed coordinations of google maps

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Google maps
 function initMap() {
   // The location of the initial marker
-  const location = { lat: -25.344, lng: 131.036 }; // Example coordinates (Uluru, Australia)
+  const location = { lat: 55.6050, lng: 13.0038 }; // Example coordinates (Uluru, Australia)
   // The map, centered at the location
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
