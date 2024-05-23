69dc1de0d5593b8ca2bceeac511467005ff557dd
Erik Berger <berger.erik12@gmail.com>
2024-05-23 13:09:12 +0200

    Including google maps in footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("Life, The Universe and Everything!");
+// Google maps
+function initMap() {
+  // The location of the initial marker
+  const location = { lat: -25.344, lng: 131.036 }; // Example coordinates (Uluru, Australia)
+  // The map, centered at the location
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location,
+  });
+  // The marker, positioned at the location
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}
