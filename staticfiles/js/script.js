399c1ac3fea8d0fc1d58855d8ef31c868748ec4b
Erik Berger <berger.erik12@gmail.com>
2024-05-27 14:17:09 +0200

    New static files deployed with collectstatic

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,1 +1,15 @@
-console.log("Life, The Universe and Everything!");
+// Google maps
+function initMap() {
+  // The location of the initial marker
+  const location = { lat: 55.6050, lng: 13.0038 }; // Example coordinates (Uluru, Australia)
+  // The map, centered at the location
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location,
+  });
+  // The marker, positioned at the location
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}
