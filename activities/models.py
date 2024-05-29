70b2f120a6157fb9f8893c5643e8c06ebeae2325
Erik Berger <berger.erik12@gmail.com>
2024-05-29 11:58:47 +0200

    Added category model for admin dashboard

--- activities/models.py
+++ activities/models.py
@@ -3,12 +3,42 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+class Category(models.Model):
+    BOXING = 'Boxing'
+    THAI_BOXING = 'Thai Boxing'
+    BJJ = 'BJJ'
+    PRIVATE_TRAINING = 'Private Training'
+    GROUP_TRAINING = 'Group Training'
+    EVENTS = 'Events'
+    CORPORATE_EVENTS = 'Corporate Events'
+    CHILDREN_CLASSES = 'Children Classes'
+
+    CATEGORY_CHOICES = [
+        (BOXING, 'Boxing'),
+        (THAI_BOXING, 'Thai Boxing'),
+        (BJJ, 'BJJ'),
+        (PRIVATE_TRAINING, 'Private Training'),
+        (GROUP_TRAINING, 'Group Training'),
+        (EVENTS, 'Events'),
+        (CORPORATE_EVENTS, 'Corporate Events'),
+        (CHILDREN_CLASSES, 'Children Classes'),
+    ]
+
+    name = models.CharField(
+        max_length=100, choices=CATEGORY_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class TrainingSession(models.Model):
     title = models.CharField(max_length=200, default='')
     instructor = models.CharField(max_length=100)
     date = models.DateField()
     time = models.TimeField()
     featured_image = CloudinaryField('image', default='placeholder')
+    categories = models.ManyToManyField(
+        Category, related_name='training_sessions')
     description = models.TextField(default='')
     excerpt = models.TextField(max_length=500, blank=True)
 
