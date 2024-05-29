ccd04669f66b818d24bb27069769e419494dcaa8
Erik Berger <berger.erik12@gmail.com>
2024-05-29 19:27:07 +0200

    Styling image container on home-page

--- muaythai/settings.py
+++ muaythai/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
