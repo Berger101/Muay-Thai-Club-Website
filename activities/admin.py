7cd6afe550efcb38081ec520d7ff4912fc88553a
Erik Berger <berger.erik12@gmail.com>
2024-05-29 12:04:50 +0200

    Views to handle category filtering and ensure the template path is correct.

--- activities/admin.py
+++ activities/admin.py
@@ -1,16 +1,30 @@
 from django.contrib import admin
-from .models import TrainingSession, Booking
+from .models import TrainingSession, Category, Booking
 from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
 
 
 @admin.register(TrainingSession)
 class TrainingSessionAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('description',)
-    list_display = ('instructor', 'date', 'time')
+    list_display = ('title', 'instructor', 'date', 'time')
     search_fields = ['instructor', 'description']
-    list_filter = ('instructor', 'date')
+    list_filter = ('title', 'instructor', 'date', 'categories')
     prepopulated_fields = {'title': ('instructor', 'date')}
+    filter_horizontal = ('categories',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['categories'].queryset = Category.objects.filter(
+            name__in=[Category.BOXING, Category.THAI_BOXING, Category.BJJ,
+                      Category.PRIVATE_TRAINING, Category.GROUP_TRAINING, Category.EVENTS, Category.CORPORATE_EVENTS, Category.CHILDREN_CLASSES]
+        )
+        return form
 
 
 class BookingAdmin(admin.ModelAdmin):
--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import TrainingSession
+from .models import TrainingSession, Category
 
 
 class TrainingSessionListView(ListView):
@@ -8,6 +8,19 @@ class TrainingSessionListView(ListView):
     template_name = 'index.html'
     paginate_by = 6
     context_object_name = 'sessions'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_name = self.request.GET.get('category', None)
+        if category_name:
+            category = get_object_or_404(Category, name=category_name)
+            queryset = queryset.filter(categories=category)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
 
 
 class TrainingSessionDetailView(DetailView):
