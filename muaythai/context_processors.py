7d0212c4936a27a02a5bc772ee2c6bc3951fd6bc
Erik Berger <berger.erik12@gmail.com>
2024-06-04 14:50:54 +0200

    Updated context processor with settings to send email

--- muaythai/context_processors.py
+++ muaythai/context_processors.py
@@ -5,3 +5,11 @@ def google_maps_api_key(request):
     return {
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     }
+
+
+def emailjs_settings(request):
+    return {
+        'EMAILJS_SERVICE_ID': settings.EMAILJS_SERVICE_ID,
+        'EMAILJS_TEMPLATE_ID': settings.EMAILJS_TEMPLATE_ID,
+        'EMAILJS_PUBLIC_KEY': settings.EMAILJS_PUBLIC_KEY,
+    }
