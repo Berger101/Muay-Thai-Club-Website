938bc729b41f487afcca62ea09976b442314b008
Erik Berger <berger.erik12@gmail.com>
2024-06-05 11:43:01 +0200

    Removed sending emails to inbox

--- muaythai/context_processors.py
+++ muaythai/context_processors.py
@@ -5,11 +5,3 @@ def google_maps_api_key(request):
     return {
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     }
-
-
-def emailjs_settings(request):
-    return {
-        'EMAILJS_SERVICE_ID': settings.EMAILJS_SERVICE_ID,
-        'EMAILJS_TEMPLATE_ID': settings.EMAILJS_TEMPLATE_ID,
-        'EMAILJS_PUBLIC_KEY': settings.EMAILJS_PUBLIC_KEY,
-    }
