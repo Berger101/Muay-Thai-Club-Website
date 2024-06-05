c0c572052832b26d9d32e4fa10e39752df2daf32
Erik Berger <berger.erik12@gmail.com>
2024-06-05 15:38:07 +0200

    Views in a function for hero image slider

--- club/views.py
+++ club/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 
 
 def home(request):
+    carousel_items = [
+        {
+            'image': 'images/muaythai_homeimage.jpg',
+            'alt': 'Muay Thai Home Image',
+            'heading': 'Welcome to the Muay Thai Club',
+            'description': 'This is the home page of the Muay Thai club. Here you can find information about our activities, training sessions, and more.'
+        },
+        {
+            'image': 'images/events.jpg',
+            'alt': 'Events',
+            'heading': 'Welcome to the Muay Thai Club',
+            'description': 'This is the home page of the Muay Thai club. Here you can find information about our activities, training sessions, and more.'
+        },
+        {
+            'image': 'images/kidstraining.jpg',
+            'alt': 'Kids Training',
+            'heading': 'Welcome to the Muay Thai Club',
+            'description': 'This is the home page of the Muay Thai club. Here you can find information about our activities, training sessions, and more.'
+        },
+    ]
+
     activities = [
         {
             'title': 'Thai Boxing',
@@ -44,4 +65,4 @@ def home(request):
             'link': '/activities/category/events/',
         },
     ]
-    return render(request, 'club/home.html', {'activities': activities})
+    return render(request, 'club/home.html', {'carousel_items': carousel_items, 'activities': activities})
