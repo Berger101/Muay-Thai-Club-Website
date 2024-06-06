ad6db2ec0c755ccdf9255fe85c82a173cdfa6bee
Erik Berger <berger.erik12@gmail.com>
2024-06-06 15:21:22 +0200

    Created my views for booking and deleting a activity

--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
-from .models import TrainingSession, Category
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from .models import TrainingSession, Category, Booking
+from .forms import BookingForm
 
 
 class TrainingSessionListView(ListView):
@@ -27,3 +29,35 @@ class TrainingSessionDetailView(DetailView):
     model = TrainingSession
     template_name = 'training_session_detail.html'
     context_object_name = 'session'
+
+
+
+class BookingListView(ListView):
+    model = Booking
+    template_name = 'booking_list.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
+
+
+class BookingCreateView(CreateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = 'booking_form.html'
+    success_url = reverse_lazy('booking_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.session = get_object_or_404(
+            TrainingSession, id=self.request.POST.get('session'))
+        return super().form_valid(form)
+
+
+class BookingDeleteView(DeleteView):
+    model = Booking
+    template_name = 'booking_confirm_delete.html'
+    success_url = reverse_lazy('booking_list')
+
+    def get_queryset(self):
+        return Booking.objects.filter(user=self.request.user)
