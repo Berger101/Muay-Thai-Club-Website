585daea3b1b49b8b74de74c112a62ee70558e292
Erik Berger <berger.erik12@gmail.com>
2024-06-06 15:19:36 +0200

    Importing urls paths for booking a activity

--- activities/urls.py
+++ activities/urls.py
@@ -1,11 +1,14 @@
 from . import views
 from django.urls import path
-from .views import TrainingSessionListView, TrainingSessionDetailView
+from .views import TrainingSessionListView, TrainingSessionDetailView, BookingListView, BookingCreateView, BookingDeleteView
 
 
-#  urlpattern for TrainingSessionList class-based view (from activities/views.py) named home.
 urlpatterns = [
     path('', TrainingSessionListView.as_view(), name='activities'),
     path('activities/<int:pk>/', TrainingSessionDetailView.as_view(), name='training_session_detail'),
     path('category/<slug:category_slug>/', TrainingSessionListView.as_view(), name='category_sessions'),
+    # CRUD (create, read, delete) bookings
+    path('bookings/', BookingListView.as_view(), name='booking_list'),
+    path('bookings/new/', BookingCreateView.as_view(), name='booking_create'),
+    path('bookings/<int:pk>/delete/', BookingDeleteView.as_view(), name='booking_delete'),
 ]
