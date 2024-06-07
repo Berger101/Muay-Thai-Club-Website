beeed64d82b01409a13564152fbf53184d930901
Erik Berger <berger.erik12@gmail.com>
2024-06-07 16:51:37 +0200

    Url paths for creating full CRUD for admins

--- activities/urls.py
+++ activities/urls.py
@@ -1,12 +1,19 @@
 from . import views
 from django.urls import path
-from .views import TrainingSessionListView, TrainingSessionDetailView, BookingListView
+from .views import (
+    TrainingSessionListView, TrainingSessionDetailView, BookingListView,
+    TrainingSessionCreateView, TrainingSessionUpdateView, TrainingSessionDeleteView
+)
 
 
 urlpatterns = [
     path('', TrainingSessionListView.as_view(), name='activities'),
     path('activities/<int:pk>/', TrainingSessionDetailView.as_view(), name='training_session_detail'),
     path('category/<slug:category_slug>/', TrainingSessionListView.as_view(), name='category_sessions'),
-    # CRUD (create, read, delete) bookings
+    # Bookings
     path('bookings/', BookingListView.as_view(), name='booking_list'),
+    # CRUD (create, read, delete) activities
+    path('activities/new/', TrainingSessionCreateView.as_view(), name='training_session_create'),
+    path('activities/<int:pk>/edit/', TrainingSessionUpdateView.as_view(), name='training_session_edit'),
+    path('activities/<int:pk>/delete/', TrainingSessionDeleteView.as_view(), name='training_session_delete'),
 ]
