4d4aa392f331704d5270186919ef02158f662875
Erik Berger <berger.erik12@gmail.com>
2024-06-09 18:41:22 +0200

    Removed unused code

--- activities/urls.py
+++ activities/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('', TrainingSessionListView.as_view(), name='activities'),
     path('activities/<int:pk>/', TrainingSessionDetailView.as_view(), name='training_session_detail'),
     path('category/<slug:category_slug>/', TrainingSessionListView.as_view(), name='category_sessions'),
-    # Bookings
-    # path('bookings/', BookingListView.as_view(), name='booking_list'),
     # CRUD (create, read, delete) activities
     path('activities/new/', TrainingSessionCreateView.as_view(), name='training_session_create'),
     path('activities/<int:pk>/edit/', TrainingSessionUpdateView.as_view(), name='training_session_edit'),
