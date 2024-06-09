ad0b6e85e62ced29eda294b64f55cf90d5c442d8
Erik Berger <berger.erik12@gmail.com>
2024-06-09 14:50:35 +0200

    Removed unnecessary paths

--- activities/urls.py
+++ activities/urls.py
@@ -1,7 +1,6 @@
-from . import views
 from django.urls import path
 from .views import (
-    TrainingSessionListView, TrainingSessionDetailView, BookingListView,
+    TrainingSessionListView, TrainingSessionDetailView,
     TrainingSessionCreateView, TrainingSessionUpdateView, TrainingSessionDeleteView
 )
 
@@ -11,7 +10,7 @@ urlpatterns = [
     path('activities/<int:pk>/', TrainingSessionDetailView.as_view(), name='training_session_detail'),
     path('category/<slug:category_slug>/', TrainingSessionListView.as_view(), name='category_sessions'),
     # Bookings
-    path('bookings/', BookingListView.as_view(), name='booking_list'),
+    # path('bookings/', BookingListView.as_view(), name='booking_list'),
     # CRUD (create, read, delete) activities
     path('activities/new/', TrainingSessionCreateView.as_view(), name='training_session_create'),
     path('activities/<int:pk>/edit/', TrainingSessionUpdateView.as_view(), name='training_session_edit'),
